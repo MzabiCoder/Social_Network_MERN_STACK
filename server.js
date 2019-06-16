554fdbb21b434b686cff225506ea5631ef0f5d45
Nabil Fannane <nabil@goadroit.com>
2019-06-15 18:36:22 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hellow WOrld')
+})
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`)
+})
